3c0b12a5ab68bbac9dfad576acd53ce2d2183de8
Chshine1 <chshine@qq.com>
2024-04-09 18:04:22 +0800

    fix:修复了编辑器中编辑内容不会保存的问题

--- DiaryProject/Models/EditableMemoModel.cs
+++ DiaryProject/Models/EditableMemoModel.cs
@@ -12,8 +12,7 @@ public class EditableMemoModel : BindableBase
     private static readonly string[] CategoryDisplays = ["事件类别：未分类", "事件类别：日记", "事件类别：提醒", "事件类别：闹钟", "事件类别：备忘录"];
     
     private bool _status;
-    private MemoRecord _memo;
-    
+
     #region Properties
 
     public bool Status
@@ -27,25 +26,13 @@ public class EditableMemoModel : BindableBase
         }
     }
 
-    public MemoRecord Memo
-    {
-        get => _memo;
-        set
-        {
-            _memo = value;
-            ContentDocument = _memo.Content.ConvertToFlowDocument();
-            ContentDocument.Focusable = true;
-            ContentDocument.LineHeight = 1;
-            RaisePropertyChanged();
-            RaisePropertyChanged(nameof(ContentDocument));
-        }
-    }
+    public MemoRecord Memo { get; }
 
-    public FlowDocument? ContentDocument { get; private set; }
+    public FlowDocument ContentDocument { get; set; }
 
     public string StartTimeHour
     {
-        get => $"{_memo.StartTime.Hour:00}";
+        get => $"{Memo.StartTime.Hour:00}";
         set
         {
             var hour = 0;
@@ -53,20 +40,20 @@ public class EditableMemoModel : BindableBase
             catch (Exception) { /* ignored */ }
             hour = Math.Min(Math.Max(hour, 0), 23);
             
-            _memo.StartTime = new DateTime(_memo.StartTime.Year, _memo.StartTime.Month, _memo.StartTime.Day, hour,
-                _memo.StartTime.Minute, 0);
-            if (_memo.StartTime >= _memo.EndTime && _memo.StartTime is { Hour: 23, Minute: 59 })
+            Memo.StartTime = new DateTime(Memo.StartTime.Year, Memo.StartTime.Month, Memo.StartTime.Day, hour,
+                Memo.StartTime.Minute, 0);
+            if (Memo.StartTime >= Memo.EndTime && Memo.StartTime is { Hour: 23, Minute: 59 })
             {
-                _memo.StartTime = new DateTime(_memo.StartTime.Year, _memo.StartTime.Month, _memo.StartTime.Day, 23,
+                Memo.StartTime = new DateTime(Memo.StartTime.Year, Memo.StartTime.Month, Memo.StartTime.Day, 23,
                     58, 0);
-                _memo.EndTime = _memo.StartTime.AddMinutes(1);
+                Memo.EndTime = Memo.StartTime.AddMinutes(1);
                 RaisePropertyChanged(nameof(StartTimeMinute));
                 RaisePropertyChanged(nameof(EndTimeHour));
                 RaisePropertyChanged(nameof(EndTimeMinute));
             }
-            else if (_memo.StartTime >= _memo.EndTime)
+            else if (Memo.StartTime >= Memo.EndTime)
             {
-                _memo.EndTime = _memo.StartTime.AddMinutes(1);
+                Memo.EndTime = Memo.StartTime.AddMinutes(1);
                 RaisePropertyChanged(nameof(EndTimeHour));
                 RaisePropertyChanged(nameof(EndTimeMinute));
             }
@@ -75,7 +62,7 @@ public class EditableMemoModel : BindableBase
 
     public string StartTimeMinute
     {
-        get => $"{_memo.StartTime.Minute:00}";
+        get => $"{Memo.StartTime.Minute:00}";
         set
         {
             var minute = 0;
@@ -83,19 +70,19 @@ public class EditableMemoModel : BindableBase
             catch (Exception) { /* ignored */ }
             minute = Math.Min(Math.Max(minute, 0), 59);
             
-            _memo.StartTime = new DateTime(_memo.StartTime.Year, _memo.StartTime.Month, _memo.StartTime.Day, _memo.StartTime.Hour,
+            Memo.StartTime = new DateTime(Memo.StartTime.Year, Memo.StartTime.Month, Memo.StartTime.Day, Memo.StartTime.Hour,
                 minute, 0);
-            if (_memo.StartTime >= _memo.EndTime && _memo.StartTime is { Hour: 23, Minute: 59 })
+            if (Memo.StartTime >= Memo.EndTime && Memo.StartTime is { Hour: 23, Minute: 59 })
             {
-                _memo.StartTime = new DateTime(_memo.StartTime.Year, _memo.StartTime.Month, _memo.StartTime.Day, 23,
+                Memo.StartTime = new DateTime(Memo.StartTime.Year, Memo.StartTime.Month, Memo.StartTime.Day, 23,
                     58, 0);
-                _memo.EndTime = _memo.StartTime.AddMinutes(1);
+                Memo.EndTime = Memo.StartTime.AddMinutes(1);
                 RaisePropertyChanged(nameof(EndTimeHour));
                 RaisePropertyChanged(nameof(EndTimeMinute));
             }
-            else if (_memo.StartTime >= _memo.EndTime)
+            else if (Memo.StartTime >= Memo.EndTime)
             {
-                _memo.EndTime = _memo.StartTime.AddMinutes(1);
+                Memo.EndTime = Memo.StartTime.AddMinutes(1);
                 RaisePropertyChanged(nameof(EndTimeHour));
                 RaisePropertyChanged(nameof(EndTimeMinute));
             }
@@ -104,7 +91,7 @@ public class EditableMemoModel : BindableBase
     
     public string EndTimeHour
     {
-        get => $"{_memo.EndTime.Hour:00}";
+        get => $"{Memo.EndTime.Hour:00}";
         set
         {
             var hour = 0;
@@ -112,14 +99,14 @@ public class EditableMemoModel : BindableBase
             catch (Exception) { /* ignored */ }
             hour = Math.Min(Math.Max(hour, 0), 23);
             
-            _memo.EndTime = new DateTime(_memo.EndTime.Year, _memo.EndTime.Month, _memo.EndTime.Day, hour,
-                _memo.EndTime.Minute, 0);
+            Memo.EndTime = new DateTime(Memo.EndTime.Year, Memo.EndTime.Month, Memo.EndTime.Day, hour,
+                Memo.EndTime.Minute, 0);
         }
     }
 
     public string EndTimeMinute
     {
-        get => $"{_memo.EndTime.Minute:00}";
+        get => $"{Memo.EndTime.Minute:00}";
         set
         {
             var minute = 0;
@@ -127,28 +114,28 @@ public class EditableMemoModel : BindableBase
             catch (Exception) { /* ignored */ }
             minute = Math.Min(Math.Max(minute, 0), 59);
             
-            _memo.EndTime = new DateTime(_memo.EndTime.Year, _memo.EndTime.Month, _memo.EndTime.Day, _memo.EndTime.Hour,
+            Memo.EndTime = new DateTime(Memo.EndTime.Year, Memo.EndTime.Month, Memo.EndTime.Day, Memo.EndTime.Hour,
                 minute, 0);
         }
     }
 
-    public string HeaderColor => HeaderColors[_memo.Category - 1];
+    public string HeaderColor => HeaderColors[Memo.Category - 1];
 
     public int CategoryIndex
     {
-        get => _memo.Category - 1;
+        get => Memo.Category - 1;
         set
         {
-            _memo.Category = value + 1;
+            Memo.Category = value + 1;
             RaisePropertyChanged(nameof(CategoryText));
             RaisePropertyChanged(nameof(HeaderColor));
             RaisePropertyChanged(nameof(CategoryDisplay));
         }
     }
 
-    public string CategoryText => CategoryTexts[_memo.Category - 1];
+    public string CategoryText => CategoryTexts[Memo.Category - 1];
 
-    public string CategoryDisplay => CategoryDisplays[_memo.Category - 1];
+    public string CategoryDisplay => CategoryDisplays[Memo.Category - 1];
 
     public string StatusText => !Memo.Active ? "状态：未激活" : Status ? "状态：进行中" : "状态：未开始";
 
@@ -158,8 +145,8 @@ public class EditableMemoModel : BindableBase
 
     public EditableMemoModel(MemoRecord memo)
     {
-        _memo = memo;
-        ContentDocument = _memo.Content.ConvertToFlowDocument();
+        Memo = memo;
+        ContentDocument = Memo.Content.ConvertToFlowDocument();
         ContentDocument.Focusable = true;
         ContentDocument.LineHeight = 1;
     }
