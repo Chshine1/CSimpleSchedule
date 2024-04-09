d0a2df95bad2e499f4f0602120a0258a5fa812c8
Chshine1 <chshine@qq.com>
2024-04-09 16:35:37 +0800

    refactor:将悬浮球添加日志的逻辑移到了视图模型

--- DiaryProject/ViewModels/HoverViewModel.cs
+++ DiaryProject/ViewModels/HoverViewModel.cs
@@ -1,13 +1,23 @@
 using System.Windows;
 using System.Windows.Forms.VisualStyles;
+using DiaryProject.Service.Local;
+using DiaryProject.Service.Web;
+using DiaryProject.Shared.Dtos;
 using DiaryProject.Shared.Utils;
+using DiaryProject.Utils;
 using DiaryProject.Views;
 using Application = System.Windows.Application;
+using DragEventArgs = System.Windows.DragEventArgs;
 
 namespace DiaryProject.ViewModels;
 
 public class HoverViewModel : BindableBase
 {
+    private readonly IEventAggregator _aggregator;
+    private readonly FileCopyService _fileService;
+    private readonly IMemoService _memoService;
+    private readonly IMemoLocalRepository _memoRepository;
+
     private DateTime _targetTime;
     
     public string DateString => _targetTime.ToString("MM/dd");
@@ -19,10 +29,15 @@ public class HoverViewModel : BindableBase
     public DelegateCommand Show { get; init; }
     public DelegateCommand ToCurrentDay { get; init; }
     public DelegateCommand OpenHoverSettings { get; init; }
+    public DelegateCommand<DragEventArgs> Drop { get; init; }
     public DelegateCommand Exit { get; init; }
 
-    public HoverViewModel(IRegionManager regionManager)
+    public HoverViewModel(IMemoLocalRepository memoRepository, IMemoService memoService, FileCopyService fileService, IEventAggregator aggregator, IRegionManager regionManager)
     {
+        _memoRepository = memoRepository;
+        _memoService = memoService;
+        _fileService = fileService;
+        _aggregator = aggregator;
         _targetTime = DateTime.Now;
         NextDay = new DelegateCommand(() =>
         {
@@ -56,6 +71,58 @@ public class HoverViewModel : BindableBase
                 Application.Current.MainWindow.WindowState = WindowState.Normal;
             regionManager.Regions["MainPanel"].RequestNavigate(nameof(HoverSettingsView));
         });
+        Drop = new DelegateCommand<DragEventArgs>(arg =>
+        {
+            if (!arg.Data.GetDataPresent(typeof(string))) return;
+            try
+            {
+                var text = (string?) arg.Data.GetData(typeof(string));
+                if (string.IsNullOrEmpty(text)) return;
+                AddMemo(text);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        });
         Exit = new DelegateCommand(() => { Application.Current.Shutdown(); });
+    }
+    
+    private async void AddMemo(string content)
+    {
+        _aggregator.UpdateLoadingStatus(true);
+        var t = DateTime.Now;
+        if (t is { Hour: 23, Minute: 59 }) t = t.AddMinutes(-1);
+#if LOCAL
+        var result = await _memoRepository.AddAsync(new MemoDto
+        {
+            Id = 0,
+            Order = position,
+            Active = false,
+            Category = 1,
+            Title = string.Empty,
+            Content = string.Empty,
+            StartTime = t,
+            EndTime = t.AddMinutes(1)
+        });
+#else
+        var memoDto = new MemoDto
+        {
+            Id = 0,
+            Active = _fileService.ReadHoverConfiguration().SetActiveOnAdded,
+            Category = _fileService.ReadHoverConfiguration().DefaultMemoCategory + 1,
+            Title = content[..5],
+            Content = content,
+            StartTime = t,
+            EndTime = t.AddMinutes(1)
+        };
+        if (App.IsSynchronizing)
+        {
+            var response = await _memoService.AddAsync(memoDto);
+            App.IsSynchronizing = response.Connected;
+        }
+        await _memoRepository.AddAsync(memoDto, App.IsSynchronizing);
+#endif  
+        _aggregator.UpdateLoadingStatus(false);
     }
 }
--- DiaryProject/Views/HoverView.xaml.cs
+++ DiaryProject/Views/HoverView.xaml.cs
@@ -1,27 +1,16 @@
 using System.Windows;
 using System.Windows.Input;
 using DiaryProject.Events;
-using DiaryProject.Service.Local;
-using DiaryProject.Service.Web;
-using DiaryProject.Shared.Dtos;
-using DiaryProject.Utils;
-using DragEventArgs = System.Windows.DragEventArgs;
 
 namespace DiaryProject.Views;
 
 public partial class HoverView : Window
 {
-    private readonly FileCopyService _fileService;
     private readonly IEventAggregator _aggregator;
-    private readonly IMemoService _memoService;
-    private readonly IMemoLocalRepository _memoRepository;
 
-    public HoverView(FileCopyService fileService, IEventAggregator aggregator, IMemoService memoService, IMemoLocalRepository memoRepository)
+    public HoverView(IEventAggregator aggregator)
     {
-        _fileService = fileService;
         _aggregator = aggregator;
-        _memoService = memoService;
-        _memoRepository = memoRepository;
         InitializeComponent();
     }
 
@@ -45,63 +34,6 @@ public partial class HoverView : Window
     private void PopupOptions(object sender, MouseButtonEventArgs e)
     {
         Menu.IsOpen = true;
-    }
-
-    private void OnDragEnter(object sender, DragEventArgs e)
-    {
-    }
-
-    private void OnDrop(object sender, DragEventArgs e)
-    {
-        if (!e.Data.GetDataPresent(typeof(string))) return;
-        try
-        {
-            var text = (string?) e.Data.GetData(typeof(string));
-            if (string.IsNullOrEmpty(text)) return;
-            AddMemo(text);
-        }
-        catch (Exception exception)
-        {
-            Console.WriteLine(exception);
-        }
-    }
-    
-    private async void AddMemo(string content)
-    {
-        _aggregator.UpdateLoadingStatus(true);
-        var t = DateTime.Now;
-        if (t is { Hour: 23, Minute: 59 }) t = t.AddMinutes(-1);
-#if LOCAL
-        var result = await _memoRepository.AddAsync(new MemoDto
-        {
-            Id = 0,
-            Order = position,
-            Active = false,
-            Category = 1,
-            Title = string.Empty,
-            Content = string.Empty,
-            StartTime = t,
-            EndTime = t.AddMinutes(1)
-        });
-#else
-        var memoDto = new MemoDto
-        {
-            Id = 0,
-            Active = _fileService.ReadHoverConfiguration().SetActiveOnAdded,
-            Category = _fileService.ReadHoverConfiguration().DefaultMemoCategory + 1,
-            Title = content[..5],
-            Content = content,
-            StartTime = t,
-            EndTime = t.AddMinutes(1)
-        };
-        if (App.IsSynchronizing)
-        {
-            var response = await _memoService.AddAsync(memoDto);
-            App.IsSynchronizing = response.Connected;
-        }
-        await _memoRepository.AddAsync(memoDto, App.IsSynchronizing);
-#endif  
-        _aggregator.UpdateLoadingStatus(false);
     }
 
     private void HideHover(object sender, RoutedEventArgs e)
